e6434ae6b785e586f8773b0a1c7d63865cdc03de
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 17:42:15 +0300

    Add files via upload

---
+++ فيديو14 كورس6.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+
+//Function Declaration
+void Add(int , int );
+
+int ReadPoisitvenumber(int from, int to);
+
+int main()
+{
+    //Function Call
+    Add(4, 6);
+
+    ReadPoisitvenumber(18, 45);
+}
+
+//Function Difinition
+void Add(int a, int b)
+{
+    cout << a + b<<endl;
+}
+
+
+//example from me 
+int ReadPoisitvenumber(int from, int to)
+{
+    int Number;
+    do
+    {
+        cout << "Enter number between " << from << " and " << to << endl;
+        cin >> Number;
+    } while (Number<from || Number>to);
+
+    return Number;
+}
