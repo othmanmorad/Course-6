cbadbbe26504c70cb9a171a8212e3afb2eb23aa8
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 18:01:51 +0300

    Add files via upload

---
+++ Vedeo 25 course 6 string&cgar format printf.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char Name[] = "Mohammed Abu-Hadhoud";
+	char SchoolName[] = "Programmeg Advices";
+
+	printf("Dear %s , How are you ? \n\n", Name);
+	printf("Welcome to %s school \n\n", SchoolName);
+
+	char c = 'S';
+	printf("Setting the width of c :%1c \n" ,c);//const
+	printf("Setting the width of c :%2c \n", c);
+	printf("Setting the width of c :%*c \n", 3, c);//Dinameck
+	printf("Setting the width of c :%*c \n", 4, c);
+	printf("Setting the width of c :%*c \n", 5, c);
+}
