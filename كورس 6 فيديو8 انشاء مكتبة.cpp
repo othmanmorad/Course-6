c20f77197bb4533dd23080b818d187300aa05c1c
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 15:57:31 +0300

    Add files via upload

---
+++ كورس 6 فيديو8 انشاء مكتبة.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "MyLibrary.h";
+#include "MyInputLibrary.h";
+
+using namespace std;
+
+int main()
+{
+    MyLibrary::Teast();
+
+    cout <<"\n"<< MyLibrary::SumTowNumber(10, 20) << endl;
+
+    cout << MyInputLibrary::ReadNumber() << endl;
+}
+
