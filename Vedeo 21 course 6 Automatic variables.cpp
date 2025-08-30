438eb88afc54a7a6c35c572c0cd9cd9dece18ede
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 17:56:07 +0300

    Add files via upload

---
+++ Vedeo 21 course 6 Automatic variables.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+    auto a = 10;
+    auto y = 12.5;
+    auto z = "Othman Morad Haj-Mohammed";
+
+   cout << a<<endl;
+   cout << y<<endl;
+   cout << z<<endl;
+}
+
