62ca5d6590d0b3bd2571da600b1dd78e73ba6e03
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 17:46:28 +0300

    Add files via upload

---
+++ فيديو16 كورس6 overloading.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int Sum(double a, double b)
+{
+    return a + b;
+}
+int Sum(int a ,int b)
+{
+    return a + b;
+}
+int Sum(int a , int b ,int c)
+{
+    return a + b+c;
+}
+int Sum(int a , int b ,int c ,int d)
+{
+    return a + b+c+d;
+}
+
+
+int main()
+{
+    cout << Sum(10, 20) << endl;
+    cout << Sum(10.2 ,10.4)<<endl;
+    cout << Sum(10 ,20 ,30)<<endl;
+    cout << Sum(10 ,20,30,40)<<endl;
+}
+
