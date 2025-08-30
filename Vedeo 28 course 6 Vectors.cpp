d68cf61d8952ae4828745fb2a4b8754651d39ecd
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 18:06:20 +0300

    Add files via upload

---
+++ Vedeo 28 course 6 Vectors.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+int main()
+{
+    
+    vector <int> vNumber = { 10,20,30,40,50 };
+
+    for (int &Number : vNumber)
+    {
+        cout << Number << " ";
+    }
+    cout << endl;
+}
+
+  
