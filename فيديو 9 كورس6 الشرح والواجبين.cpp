930d855191cad7bc8b488783b1f32f19df299e1e
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 16:01:11 +0300

    Add files via upload

---
+++ فيديو 9 كورس6 الشرح والواجبين.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	//int Mark;
+	//string Result;
+	//
+	//cout << "Enter The mark :" << endl;
+	//cin >> Mark;
+	//Result = (Mark >= 50) ? "Pass" : "Faile";
+	//cout<< Result<<endl;
+
+
+
+	//Homework1
+	//int Number;
+	//string Result;
+	//cout << "Enter number :" << endl;
+	//cin >> Number;
+	//Result = (Number > 0) ? "Positive" : "Negative";
+	//cout << "Number is " << Result;
+	
+
+	//Homework2
+	int Number;
+	string Result;
+	cout << "Enter number :" << endl;
+	cin >> Number;
+	Result = (Number == 0) ? "Zero" : ((Number > 0) ? "Positive" : "Negative");
+	cout << "Number is " << Result;
+
+	return 0;
+}
+
