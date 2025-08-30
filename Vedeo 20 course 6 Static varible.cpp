f7fae73bc96893894e5bb4302b73071d4327403e
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 17:54:25 +0300

    Add files via upload

---
+++ Vedeo 20 course 6 Static varible.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void MyFunction()
+{
+   static int Number=1;
+    cout <<"value of number "<< Number << endl;
+    Number++;
+}
+int main()
+{
+     MyFunction();
+     MyFunction();
+     MyFunction();
+    return 0;
+}
+
