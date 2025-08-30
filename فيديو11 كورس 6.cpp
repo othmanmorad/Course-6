931d82b4eb04521e71fe82f7bc576a609f3fd7c3
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 17:38:14 +0300

    Add files via upload

---
+++ فيديو11 كورس 6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int ReadOnlyNumber()
+{
+    int Number;
+    cout << "Please Enter a number" << endl;
+    cin >>Number;
+
+    while (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        cout << "Invalid number ,Enter a valid one : " << endl;
+        cin >> Number;
+    }
+    return Number;
+}
+int main()
+{
+    cout << "\n  Your Number is : " << ReadOnlyNumber() << endl;
+}
+
