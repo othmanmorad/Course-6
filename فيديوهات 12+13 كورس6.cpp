7cfd9bf9d267821615be61f2efde89dca8e1457a
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 17:40:02 +0300

    Add files via upload

---
+++ فيديوهات 12+13 كورس6.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	//BitWise & operator
+	cout << "Result is " << (12 & 25) << endl;
+
+
+	//BitWise | operator
+	cout << "\nRsultt is " << (12 | 25) << endl;
+}
+
