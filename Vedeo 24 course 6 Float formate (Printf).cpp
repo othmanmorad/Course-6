04b0cda11eaa8702ef29f59db786deedc8792a37
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 17:59:54 +0300

    Add files via upload

---
+++ Vedeo 24 course 6 Float formate (Printf).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float PI = 3.14159265;
+    printf("Precision spacifaction of %.*f\n", 1, PI);
+    printf("Precision spacifaction of %.*f\n", 2, PI);
+    printf("Precision spacifaction of %.*f\n", 3, PI);
+    printf("Precision spacifaction of %.*f\n", 4, PI);
+
+    float x = 7.0, y = 9.0;
+    printf("\nThe float division is %.3F/%.3f=%.3f\n\n", x, y, x / y);
+
+    double d = 12.45;
+    printf("The double value is : %.3f \n", d);
+    printf("The double value is : %.4f \n", d);
+
+}
+
