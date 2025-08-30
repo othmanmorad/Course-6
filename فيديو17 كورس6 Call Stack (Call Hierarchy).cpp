592ed084437911b3304f59e8638888c963d9d1d1
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 17:48:31 +0300

    Add files via upload

---
+++ فيديو17 كورس6 Call Stack (Call Hierarchy).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+void Function4()
+{
+    cout << "Hi,i'm function4" << endl;
+}
+
+void Function3()
+{
+    Function4();
+}
+
+void Function2()
+{
+    Function3();
+}
+
+void Function1()
+{
+    Function2();
+    
+   
+}
+
+
+int main()
+{
+    Function1();
+}
+
