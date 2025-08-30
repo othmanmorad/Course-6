f04812c50ef1cbe4a2141f0505d8edf1352b85ce
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-08-30 18:08:06 +0300

    Add files via upload

---
+++ Vedeo 29 course س6 vectors Add elements.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+//int main()
+//{
+//
+//    vector <int> vNumber ;
+//
+//    vNumber.push_back(10);
+//    vNumber.push_back(20);
+//    vNumber.push_back(30);
+//    vNumber.push_back(40);
+//    vNumber.push_back(50);
+//
+//    for (int& Number : vNumber)
+//    {
+//        cout << Number << " ";
+//    }
+//    cout << endl;
+//}
+
+void ReadNumbers(vector <int> &vNumber)
+{
+
+	int Number;
+	char ReadMore ='Y';
+	while (ReadMore == 'Y' || ReadMore == 'y')
+	{
+		cout << "Enter the number" << endl;
+		cin >> Number;
+
+		vNumber.push_back(Number);
+
+		cout << "\nDo you want to read more numbers? Y/N ?" << endl;
+		cin >> ReadMore;
+	}
+}
+void PrintVectorNumbers(vector <int>& vNumber)
+{
+	cout << "Numbers vectors :\n\n";
+	for (int Number : vNumber)
+	{
+		cout << Number << endl;
+	}
+	cout << endl;
+}
+int main()
+{
+	vector <int> vNumber;
+	ReadNumbers(vNumber);
+	PrintVectorNumbers(vNumber);
+}
