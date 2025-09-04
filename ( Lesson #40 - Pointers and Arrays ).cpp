cc38824b99d45d8adb33efe392b35bb659eafb83
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 14:03:32 +0300

    Add files via upload

---
+++ ( Lesson #40 - Pointers and Arrays ).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[10] = { 10,20,30,40 };
+    int* p;
+    p= arr;
+
+    cout << "Adress araay : \n" << endl;
+    cout<<  p     << endl;
+    cout << p + 1 << endl;
+    cout << p + 2 << endl;
+    cout << p + 3 << endl;
+
+
+    cout << "\nValue araay : \n" << endl;
+    cout << *(p) <<  endl;
+    cout << *(p + 1) << endl;
+    cout << *(p + 2) << endl;
+    cout << *(p + 3) << endl;
+
+
+    cout << "\nAdress araay by for loop: \n" << endl;
+    for (int i = 0;i < 4;i++)
+    {
+        cout << (p + i) << endl;
+
+    }
+    cout << "\nValue araay by for loop: \n" << endl;
+    for (int i = 0;i < 4;i++)
+    {
+        cout << *(p+i) << endl;
+      
+    }
+}
+
