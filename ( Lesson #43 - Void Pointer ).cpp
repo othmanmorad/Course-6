7d5784c3d95581bf77ba497deff9f8c872177ee5
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 14:22:55 +0300

    Add files via upload

---
+++ ( Lesson #43 - Void Pointer ).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int *ptrx;
+	float* ptry;
+
+	ptrx =new int;
+	ptry = new float;
+
+	*ptrx = 45;
+	*ptry = 58.35f;
+
+	cout<<  *ptrx <<endl;
+	cout<<  *ptry << endl;
+
+	delete ptrx;
+	delete ptry;
+	cout << endl;
+}		   
+
