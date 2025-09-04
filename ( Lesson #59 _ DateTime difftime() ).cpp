d4b26ea6966164da45ef6764492e92e8ec2656b0
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 15:02:59 +0300

    Add files via upload

---
+++ ( Lesson #59 _ DateTime difftime() ).cpp
@@ -0,0 +1,17 @@
+﻿#pragma warning(disable:4996)
+#include <iostream>
+#include <ctime>
+using namespace std;
+int main()
+{
+	time_t t = time(0);
+	char* dt = ctime(&t);
+	cout << "Local data and time is : " << dt << "\n\n";
+
+	tm* gmtm = gmtime(&t);
+
+	dt = asctime(gmtm);
+	cout << "Utc data and time is : " << dt << "\n\n";
+	return 0;
+}
+
