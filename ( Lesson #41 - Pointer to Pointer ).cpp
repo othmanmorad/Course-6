9cad23d9cc4977995638722fe1e1a575f4ecc930
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 14:20:04 +0300

    Add files via upload

---
+++ ( Lesson #41 - Pointer to Pointer ).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+struct stEmployee
+{
+	string Name;
+	float Salary;
+};
+int main()
+{
+	stEmployee Employee, * ptr;
+
+	Employee.Name = "Othman Murad Haj-Mohammed";
+	Employee.Salary = 15000;
+
+	cout << Employee.Name << endl;
+	cout << Employee.Salary << endl;
+
+	cout << "\n___________________________Using Pointers____________________________\n";
+
+	ptr = &Employee;
+
+	cout << ptr->Name << endl;
+	cout << ptr->Salary << endl;
+
+	cout << &Employee.Name  << endl;
+	cout <<&Employee.Salary << endl;
+}
