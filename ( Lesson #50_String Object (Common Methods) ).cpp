89a453bf1590fa6e5a68b61ad64bf41f4f1bcb95
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 14:55:04 +0300

    Add files via upload

---
+++ ( Lesson #50_String Object (Common Methods) ).cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string s1 = "My name is Othman Morad Haj-Mohemmed , I love programming. ";
+
+	cout << s1.length() << endl;
+
+	cout << s1.at(4) << endl;
+
+	cout << s1.append(" @programmingadvices") << endl;
+
+	cout << s1.insert(7, " salih") << endl;
+
+	cout << s1.substr(17, 6) << endl;
+
+	
+		s1.push_back('x') ;
+		cout << s1 << endl;
+
+		
+				s1.pop_back();
+				cout << s1 << endl;
+		
+		
+		
+
+		cout << s1.find("Othman") << endl;
+
+		cout << s1.find("Raed") << endl;
+		if (s1.find("Raed") == s1.npos)
+		{
+			cout << "Raed not found" << endl;
+		}
+
+		s1.clear();
+		cout << s1;
+}
+
