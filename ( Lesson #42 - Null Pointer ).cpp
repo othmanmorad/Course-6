6811fbed9ef8962dba8c1b85408a027f9687f04d
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 14:21:45 +0300

    Add files via upload

---
+++ ( Lesson #42 - Null Pointer ).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	void* ptr;
+
+	float f1 = 10.5;
+	int x = 20;
+
+	ptr = &f1;
+
+	cout << ptr << endl;
+	cout << *(static_cast<float*>(ptr)) << endl;
+
+	ptr = &x;
+
+	cout << ptr << endl;
+	cout << *(static_cast<int*>(ptr)) << endl;
+
+}
+
