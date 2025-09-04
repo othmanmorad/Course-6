9ca33345cd8995e1111d8b9b0a8300a5aafb6699
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 13:37:33 +0300

    Add files via upload

---
+++ ( Lesson #32 - Struct with Vector ).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int>vNumbers;
+
+	vNumbers.push_back(10);
+	vNumbers.push_back(20);
+	vNumbers.push_back(30);
+	vNumbers.push_back(40);
+	vNumbers.push_back(50);
+
+	//vNumbers.clear();
+
+	if (vNumbers.size() > 0)
+	{
+		cout << "First element " << vNumbers.front()<<endl;
+	}
+
+	if (!vNumbers.empty())
+	{
+	  cout << "\nLast element " << vNumbers.back()<<endl;
+	}
+
+	cout << "Size " << vNumbers.size() << endl;
+
+	cout << "Capacity " << vNumbers.capacity() << endl;
+
+	cout << "Empyt " << vNumbers.empty() << endl;
+}
