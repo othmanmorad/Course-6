fd32b32cccf67d2f7a551b1fbc53f2b97603ba6c
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 14:36:50 +0300

    Add files via upload

---
+++ ( Lesson #44 - Function Pointer ).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int Number;
+	cout << "Enter total number of students" << endl;
+	cin >> Number;
+
+	float* ptr;
+	ptr = new float[Number];
+
+	cout << "Enter grades's of students" << endl;
+	for (int x = 0;x < Number;x++) 
+	{
+		cout << "Student [" << x + 1 << "]" << endl;
+		cin >> *(ptr + x);
+	}
+
+	cout << "\n Displaying grade of students" << endl;
+	for (int i = 0;i < Number;i++)
+	{
+		cout << "Student [" << i + 1 << "]  " <<*(ptr+i)<< endl;
+		
+	}
+	delete []ptr;
+}
+
