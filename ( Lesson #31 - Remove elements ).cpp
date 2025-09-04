e7b8865e003748cc7d70c0976a2abd2a72dbff07
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 13:35:03 +0300

    Add files via upload

---
+++ ( Lesson #31 - Remove elements ).cpp
@@ -0,0 +1,29 @@
+﻿// فيديو31الكورس الثالث.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	bool A = 1, B = 0;
+	cout << (A && B) << endl;
+	cout << (A || B) << endl;
+	cout << !A  << endl;
+	cout << !B << endl;
+	cout << !(A && B) << endl;
+	cout << !(A || B) << endl;
+
+
+	bool result;
+	result = !(5 > 6 || 7 == 7) && !(1 || 0);
+
+	cout << "Result = " << result << endl;
+	
+
+
+
+
+
+}
+
+
