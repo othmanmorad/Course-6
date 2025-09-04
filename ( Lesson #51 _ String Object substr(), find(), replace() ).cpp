7df38caef4d1239bea6b7e3673db8c3025d5a567
Othman Murad Haj-Mohammed <hjmhmdthmanmrad@gmail.com>
2025-09-04 14:57:06 +0300

    Add files via upload

---
+++ ( Lesson #51 _ String Object substr(), find(), replace() ).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+	char x = toupper('a');
+	char w = tolower('A');
+
+	cout << toupper('a') << endl;
+	cout << tolower('A') << endl;
+
+	cout << "converting a to A : " << x << endl;
+	cout << "converting A to a : " << w << endl;
+
+	cout << "isupper ('A') " << isupper('A') << endl;
+	cout << "islower('a') " << islower('a') << endl;
+	cout << "isdigit('5') " << isdigit('5') << endl;
+	cout << "ispunct(';') " << ispunct(';') << endl;
+
+}
+
